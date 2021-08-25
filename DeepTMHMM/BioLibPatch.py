e4deac89826c648f17a8926c5c1376fe10aeda5b
psi-cmd <psi-cmd@outlook.com>
2021-08-25 17:54:05 +0800

    patch DeepTMHMM

---
+++ DeepTMHMM/BioLibPatch.py
@@ -0,0 +1,45 @@
+from biolib.app import BioLibApp
+from biolib.biolib_binary_format import ModuleInput
+from biolib.biolib_api_client.biolib_job_api import BiolibJobApi
+from biolib.app.utils import run_job
+from biolib.biolib_api_client import JobState
+from biolib.biolib_logging import logger
+from biolib.biolib_errors import BioLibError
+
+
+class BioLibPatch(BioLibApp):
+    def __call__(self, fasta_str=None):
+        module_input_serialized = self._get_serialized_module_input(fasta_str)
+
+        job = BiolibJobApi.create(self._selected_app_version['public_id'])
+        BiolibJobApi.update_state(job['public_id'], JobState.IN_PROGRESS.value)
+
+        try:
+            module_output = run_job(job, module_input_serialized)
+
+            try:
+                BiolibJobApi.update_state(
+                    job_id=job['public_id'], state=JobState.COMPLETED.value)
+            except Exception as error:  # pylint: disable=broad-except
+                logger.warning(
+                    f'Could not update job state to completed:\n{error}')
+
+            return module_output['files']
+
+        except BioLibError as exception:
+            logger.error(f'Compute failed with: {exception.message}')
+            try:
+                BiolibJobApi.update_state(
+                    job_id=job['public_id'], state=JobState.FAILED.value)
+            except Exception as error:  # pylint: disable=broad-except
+                logger.warning(
+                    f'Could not update job state to failed:\n{error}')
+
+            raise exception
+
+    @staticmethod
+    def _get_serialized_module_input(fasta_str: str) -> bytes:
+        files_dict = {"/input.fasta": fasta_str.encode()}
+        module_input_serialized: bytes = ModuleInput().serialize(
+            stdin="", arguments=["--fasta", "input.fasta"], files=files_dict)
+        return module_input_serialized
--- DeepTMHMM/DeepTMHMM.py
+++ DeepTMHMM/DeepTMHMM.py
@@ -1,58 +1,19 @@
 #! /usr/bin/python3
 
 import json
-import os
-import biolib
-import re
-import random
-# from settings import BASE_DIR
+from BioLibPatch import BioLibPatch
 
-BASE_DIR = '/Users/haoyuanwang/Desktop/iGEM_program/'
+class DeepTMHMM:
+    def get_result(self, fasta_str: str):
+        model = BioLibPatch("DTU/DeepTMHMM")
+        return model(fasta_str)
 
-
-def parse(fasta_str: str):
-    query_protein = re.search(">(.*?)\n", fasta_str).groups()[0]
-    amino_acid_str = fasta_str.split('\n')[1]
-    return (query_protein, amino_acid_str)
-
-
-def str2json(qurey_protein: str, job_id: str):
-    result_path = BASE_DIR + 'DeepTMHMM/deeptmhmm_results/' + job_id + '/'
-    data = [{"qurey_protein": qurey_protein, "result_path": result_path}]
-    return json.dumps(data)
-
-
-def generate_job_id(length=7):
-    BASE_STR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    BASE_LEN = len(BASE_STR)
-    job_id = ''
-    for i in range(length):
-        job_id += BASE_STR[random.randint(0, BASE_LEN-1)]
-    return job_id
-
-
-def deeptmhmm(fasta_str: str):
-    deeptmhmm_tool = biolib.load('DTU/DeepTMHMM')
-    # update input.fasta file
-    os.system('touch input.fasta')
-    f = open('input.fasta', 'w')
-    f.write(fasta_str)
-    f.close()
-
-    # produce a job id randomly (string)
-    job_id = generate_job_id()
-    qurey_protein, amino_acide_str = parse(fasta_str)
-    result = deeptmhmm_tool(args='--fasta input.fasta')
-    # print(result)
-
-    # change the result directory name
-    os.system('mv biolib_results deeptmhmm_results/%s' % job_id)
-    # os.rename("biolib_results", job_id)
-    json_data = str2json(qurey_protein, job_id)
-    return json_data
+    def get_result_json(self, fasta_str: str):
+        return json.dumps(self.get_result(fasta_str))
 
 
 if __name__ == '__main__':
     test_str = """>test1
 MKMRFFSSPCGKAAVDPADRCKEDQHPMKMRFFSSPCGKAAVDPADRCKEVQQIRDQHPMKMRFFSSPCGKAAVDPADRCKEVQQKMRFFSSPCGKAADRCKEVQQIRDQHPEDQHPMKMRFFSSP"""
-    print(deeptmhmm(test_str))
+    deeptmhmm = DeepTMHMM()
+    print(deeptmhmm.get_result(test_str))
