6d62f135598005009aa24edda2380506dcbbc633
psi-cmd <psi-cmd@outlook.com>
2021-10-21 18:57:19 +0800

    fix script

--- setup.sh
+++ setup.sh
@@ -19,11 +19,11 @@ pip install -r requirements.txt
 echo "2. Move required nginx and systemd config files, assuming that systemd is used as init process"
 echo "> Note: the config differs from our deployment for https config is exclusive to specific site"
 
-cp ./server_config/cat /etc/nginx/sites-avaliable
+cp ./serve_config/cat /etc/nginx/sites-avaliable
 ln -s /etc/nginx/sites-avaliable/cat /etc/nginx/sites-enabled/cat
 rm /etc/nginx/sites-enabled/default
 
-cp ./server_config/*.service /etc/systemd/system
+cp ./serve_config/*.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start nginx.service
 systemctl start gunicorn.service
