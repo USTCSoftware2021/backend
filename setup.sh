de72ac10adf6afe8293218f47716b51f48634d16
psi-cmd <psi-cmd@outlook.com>
2021-10-21 17:51:59 +0800

    optimize setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
+
+echo "1. Create virtual environment."
+
 python3 -m venv apienv
 if ! [ -e apienv/bin/activate ]; then
+	echo "Venv create failed."
 	exit 1
 fi
 
@@ -10,4 +14,18 @@ pip install -U pip
 pip install -U setuptools
 pip install setuptools_scm
 pip install wheel
-pip install pybiolib requests requests_toolbelt flask gunicorn jpredapi scikit-learn==0.24.1 redis celery
+pip install -r requirements.txt
+
+echo "2. Move required nginx and systemd config files, assuming that systemd is used as init process"
+echo "> Note: the config differs from our deployment for https config is exclusive to specific site"
+
+cp ./server_config/cat /etc/nginx/sites-avaliable
+ln -s /etc/nginx/sites-avaliable/cat /etc/nginx/sites-enabled/cat
+rm /etc/nginx/sites-enabled/default
+
+cp ./server_config/*.service /etc/systemd/system
+systemctl daemon-reload
+systemctl start nginx.service
+systemctl start gunicorn.service
+systemctl start celery.service
+systemctl start redis.service
